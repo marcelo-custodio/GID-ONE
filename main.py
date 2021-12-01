545dc4b21f19b428609c64a9f83597bc8825f419
Marcelo Custódio <custodios.marcelo@gmail.com>
2021-11-30 22:32:08 -0300

    Removido database, alterado index.html

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import re
-from flask import Flask, flash, render_template, request
+from flask import Flask, render_template, request, redirect
 from werkzeug.utils import secure_filename
-from flask_sqlalchemy import SQLAlchemy
 from cryptography.hazmat.primitives import hashes
-from datetime import datetime as dt
 import os
 
 def resumo_criptografico(path):
@@ -14,35 +11,25 @@ def resumo_criptografico(path):
     return digest.finalize()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///resumo.db'
-app.config['UPLOAD_FOLDER'] = '/temp_files'
-db = SQLAlchemy(app)
+app.config['UPLOAD_FOLDER'] = 'temp_files'
 
-class File(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.DateTime, default=dt.utcnow)
-    name = db.Column(db.Text, nullable=False, default='')
-    summary = db.Column(db.LargeBinary, nullable=False)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
-
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
-@app.route('/upload', methods=['POST'])
-def upload():
     if request.method == 'POST':
         if 'file' not in request.files:
-            flash('Sem arquivos selecionados')
-            return render_template('index.html')
+            return redirect(request.url)
         
-        for file in request.files:
-            filename = secure_filename(file)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # TO_DO resumo criptografico
-            return render_template('index.html')
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        print(path)
+        file.save(path)
+        summary = resumo_criptografico(path)
+        print(summary)
+        os.remove(path)
+        return render_template('index.html', resumo = summary.hex(), nome=filename)
+    else:
+        return render_template('index.html', resumo='', nome='')
 
 if __name__ == '__main__':
     app.run(debug=True)
